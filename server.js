fa0e7ecfd1579c3183ec829d3ef1f5fa9546a640
peluche1275 <Nathan.altomare.belier@gmail.com>
2020-10-03 13:24:16 +0200

    Advancing in the todo  list

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // DataBase //
 
 let dataBase = [{ id: 0, todo: 'Nourrir le chat', done: false },
-                { id: 1, todo: 'Manger une banane', done: false },
-                { id: 2, todo: 'Nettoyer la litière', done: false },
-                { id: 3, todo: 'Faire à manger', done: false }]
+{ id: 1, todo: 'Manger une banane', done: false },
+{ id: 2, todo: 'Nettoyer la litière', done: false },
+{ id: 3, todo: 'Faire à manger', done: false }]
 
 // Module
 
 let express = require('express');
 let bodyparser = require('body-parser');
+let url = require('url');
 let session = require('express-session')
 
 let app = express()
@@ -26,14 +27,24 @@ app.use(bodyparser.json());
 // Route
 
 app.get('/', (req, res) => {
-    res.render('Web/index', { dataBase })
+    const queryObject = url.parse(req.url,true).query;
+
+    if(queryObject.del != undefined){
+        let dataBaseIndexById = dataBase.findIndex(x => x.id == queryObject.del);
+        dataBase[dataBaseIndexById].done = true;
+    }
+
+    res.render('web/index', { dataBase })
 })
+
+
 
 app.post('/', (req, res) => {
-    dataBase.push({id:dataBase.length,todo:req.body.message,done:false})
+    dataBase.push({ id: dataBase.length, todo: req.body.message, done: false })
+    res.render('web/index', { dataBase })
 })
 
-app.listen(3000);
+app.listen(8080);
 
 
 
